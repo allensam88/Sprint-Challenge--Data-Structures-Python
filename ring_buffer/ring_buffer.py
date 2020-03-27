f73eb41494b0cbe31c63e71e1ee22240dcc24c54
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-03-27 13:24:32 -0700

    getting closer, but still failing last couple tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,23 +4,26 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
 	def __init__(self, capacity):
 		self.capacity = capacity
-		self.current = None
+		self.current_head = None
 		self.storage = DoublyLinkedList()
 
 	def append(self, item):
+		# ---deprecated---
 		# empty, add item to head
-		if self.storage.length == 0:
-			self.storage.add_to_head(item)
+		# if self.storage.length == 0:
+		# 	self.storage.add_to_head(item)
+		# 	self.current_head = self.storage.head
 
-		# full, remove from tail, then add to head
-		elif self.storage.length == self.capacity:
-			self.storage.remove_from_head()
-			self.storage.add_to_head(item)
+		# if full, remove from tail, then add to head
+		if self.storage.length == self.capacity:
+			self.current.value = item
+			self.current = self.current.next
+			self.storage.tail = self.current
 
-		# space remaining, add to tail
+		# if empty or space remaining, add to tail, reset current
 		elif self.storage.length < self.capacity:
 			self.storage.add_to_tail(item)
-			self.current = item
+			self.current = self.storage.head
 
 	def get(self):
 		# Note:  This is the only [] allowed
@@ -42,11 +45,27 @@ buffer.append('a')
 buffer.append('b')
 buffer.append('c')
 buffer.append('d')
-print(buffer.get())
+print(buffer.get()) # should return ['a', 'b', 'c', 'd']
 buffer.append('e')
-print(buffer.get())
+print(buffer.get()) # should return ['a', 'b', 'c', 'd', 'e']
 buffer.append('f')
-print(buffer.get())
+print(buffer.current.value)
+print(buffer.current.next.value)
+print(buffer.get()) # should return ['f', 'b', 'c', 'd', 'e']
+buffer.append('g')
+print(buffer.current.value)
+print(buffer.current.next.value)
+print(buffer.get()) # should return ['f', 'g', 'c', 'd', 'e']
+buffer.append('h')
+print(buffer.current.value)
+print(buffer.current.next.value)
+buffer.append('i')
+print(buffer.current.value)
+# print(buffer.current.next.value)
+print(buffer.get()) # should return ['f', 'g', 'h', 'i', 'e']
+buffer.append('j')
+buffer.append('k')
+print(buffer.get()) # should return ['k', 'g', 'h', 'i', 'j']
 
 # ----------------Stretch Goal-------------------
 
