2b345eb40b65a914478c55fb23f541ea2c09a35b
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-03-27 08:26:38 -0700

    got names done with BST import

---
+++ names/binary_search_tree.py
@@ -0,0 +1,37 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        # if value == self.value:
+        #     return ?
+        if value < self.value:
+            if self.left:
+                return self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+        else:
+            if self.right:
+                return self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+        else:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
